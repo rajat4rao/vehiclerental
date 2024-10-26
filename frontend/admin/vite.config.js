36f12cf5d973c3dca258e8d23d0b36ac1a4caac2
rajat4rao <balprao@gmail.com>
2024-10-27 01:09:37 +0530

    vite

--- frontend/admin/vite.config.js
+++ frontend/admin/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/src/main.jsx',
-    },
-  },
-  
 })
