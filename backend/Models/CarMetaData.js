b9af4a92e82b48fc775ba30ebd2cad490a55bb59
rajat4rao <balprao@gmail.com>
2024-10-27 10:28:10 +0530

    schema

--- backend/Models/CarMetaData.js
+++ backend/Models/CarMetaData.js
@@ -1,10 +1,11 @@
 const {mongoose}=require('mongoose')
 
-const Schema=mongoose.Schema({
-    Fuel:Array,
-    Model:Array,
-    Make:Array
-})
+const Schema = mongoose.Schema({
+    Fuel: Array,
+    Model: Array,
+    Make: Array,
+    Type: Array,
+  });
 
 const CarMetaData=mongoose.model("carmetadatas",Schema)
 
